4c5e7db0e57587f6215ba5b2bb219495fa543888
ManyEmanuel <fmanuel.gutierrez@gmail.com>
2024-08-06 13:26:15 -0700

    MannyJustificante

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -10,7 +10,7 @@ import axios from "axios";
 // for each client)
 
 const api = axios.create({
-  baseURL: "http://sistema.ieenayarit.org:9170/api",
+  baseURL: "http://sistema.ieenayarit.org:9270/api",
   //baseURL: "http://sistema.ieenayarit.org:9270/api",
 });
 // const api = axios.create({
